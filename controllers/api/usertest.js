7872c24bacaffe24a244ba999547c814cc2b87d8
petermc87 <peter.mcgibney@gmail.com>
2023-08-23 17:04:12 -0400

    Update files.

--- controllers/api/usertest.js
+++ controllers/api/usertest.js
@@ -1,7 +1,7 @@
-// const e = require('express')
 const Usertest = require('../../models/restaurant/usertest')
 const Blog = require('../../models/restaurant/blog')
 require('../../models/restaurant/blog')
+
 const dataController = {
   async addBlog (req, res) {
     try {
@@ -28,19 +28,6 @@ const dataController = {
     })
   },
 
-  // addBlog (req, res, next) {
-  //   Usertest.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedUsertest) => {
-  //     if (err) {
-  //       res.status(400).send({
-  //         msg: err.message
-  //       })
-  //     } else {
-  //       updatedUsertest.populate("blogs").exec()
-  //       res.locals.data.user = updatedUsertest
-  //       next()
-  //     }
-  //   })
-  // },
   create (req, res, next) {
     Usertest.create(req.body, (err, createdUser) => {
       if (err) {
