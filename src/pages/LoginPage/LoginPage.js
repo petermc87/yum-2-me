18aba8bf650e7fc3699ed0514e74808de544d647
petermc87 <peter.mcgibney@gmail.com>
2023-10-01 21:15:23 -0400

    feat: add sample accounts

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,21 +1,42 @@
 import LoginForm from '../../components/LoginForm/LoginForm'
-import { useEffect } from 'react'
-
 export default function LoginPage (props) {
   return (
     <>
+        <div>
+        <h2>Try out a sample account!</h2>
+          <strong>Customer:</strong>
+          <br />
+          Email: j@j.com
+          <br />
+          Password: 12345
+        <br/>
+        <br/>
+          <strong>Driver:</strong>
+            <br />
+            Email: jerry@mail.com
+            <br />
+            Password: 12345
+        <br/>
+        <br/>
+          <strong>Restaurant:</strong>
+            <br />
+            Email: me@gmail.com
+            <br />
+            Password: 12345
+      </div>
+
+      <h2>~ OR ~</h2>
       <h1>Login</h1>
       <LoginForm
         setUser={props.setUser}
         user={props.user}
-
         getCustomer={props.getCustomer}
-
         getRestaurantsByUser={props.getRestaurantsByUser}
         setRestaurantsByUser={props.setRestaurantsByUser}
-
         foundCustomer={props.foundCustomer}
       />
+
     </>
+     
   )
 }
